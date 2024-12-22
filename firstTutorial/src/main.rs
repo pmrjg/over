0e7c96df6f7ed5c0b64605ef86b27e4d9752d852
Pedro Jorge <pmrjge@gmail.com>
2024-12-22 17:37:50 +0000

    Main function is in order

--- firstTutorial/src/main.rs
+++ firstTutorial/src/main.rs
@@ -22,7 +22,58 @@ use winit::window::{Window, WindowBuilder};
 use std::sync::Arc;
 
 
-
 fn main() {
-    println!("Hello, world!");
+
+    // instance
+    let instance = {
+        let library = VulkanLibrary::new().unwrap();
+        let extensions = vulkano_win::required_extensions(&library);
+
+        Instance::new(library,
+            InstanceCreateInfo {
+                enabled_extensions: extensions,
+                enumerate_portability: true,
+                max_api_version: Some(Version::V1_1),
+                ..Default::default()
+            }
+        )
+            .unwrap()
+    };
+
+    // UI Window
+    let event_loop = EventLoop::new();
+    let surface = WindowBuilder::new().build_vk_surface(&event_loop, instance.clone()).unwrap();
+
+    // Physical Device and Queue Family
+    let device_extensions = DeviceExtensions {
+        khr_swapchain: true,
+        ..DeviceExtensions::empty()
+    };
+
+    let (physical_device, queue_family_index) = instance.enumerate_physical_devices().unwrap()
+        .filter(|p| p.supported_extensions().contains(&device_extensions))
+        .fiter_map(
+            |p| {
+                p.queue_family_properties()
+                    .iter()
+                    .enumerate()
+                    .position(|i, q| {
+                        q.queue_flags.properties && p.surface(i as u32, &surface).unwrap_or(false)
+                    })
+                    .map(|i| (p, i as u32))
+            }
+        )
+        .min_by_key(|p, _|{
+            match p.properties().device_type {
+                PhysicalDeviceType::DiscreteGpu => 0,
+                PhysicalDeviceType::IntegratedGpu => 1,
+                PhysicalDeviceType::VirtualGpu => 2,
+                PhysicalDeviceType::Cpu => 3,
+                PhysicalDeviceType::Other => 4,
+                _ => 5
+            }
+        })
+        .expect("No suitable physical device found.");
+
+    // Device
 }
